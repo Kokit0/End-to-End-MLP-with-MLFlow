96d708db4f29d00363d74e7367ca0144ab4266b7
Kokke <jorge.amaya.s@gmail.com>
2023-09-21 21:53:37 -0300

    actrualización de experimento con 01_data_ingestion.ipýnb, creación códeblocks para ponlar folder /file creation

---
+++ src/mlProject/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+#<---recordar que resolvimos el conflicto del forward slash para automatizar mejor el proceso.
+#<---por lo que de ahora en adelante, la busqueda de rutas/path no necesitará de cambios manuales del backslash a forward slash.
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
