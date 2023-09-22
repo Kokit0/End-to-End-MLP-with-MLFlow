f6c173c0cf264bad038af0d0031cfc1ee954f295
Kokke <jorge.amaya.s@gmail.com>
2023-09-21 22:25:23 -0300

    polish del agregado de Data ingestion

---
+++ src/mlProject/entity/config_entity.py
@@ -0,0 +1,11 @@
+#entity dataclass
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True) #<--- esta entidad, mi dataclass para config ingestion ligado al config.yaml. no es una python class si no que una dataclass. como version escrita de esta funcion.
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
---
+++ src/mlProject/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,31 @@
+from mlProject.config.configuration import ConfigurationManager
+from mlProject.components.data_ingestion import DataIngestion
+from mlProject import logger
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionTrainingPipeline: #<-- El pipeline que diseñé en mi data_ingestion notebook
+    def __init__(self):
+        pass
+
+    def main(self): #<-- El main method que llamaré desde main.py en carpeta raiz
+       config = ConfigurationManager() #inicio mi ConfigurationManager
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file() #llamo el metodo de download
+        data_ingestion.extract_zip_file()# llamo el metodo de extracción
+
+
+    
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
